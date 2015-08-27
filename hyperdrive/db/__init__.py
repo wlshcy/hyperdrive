7c6d41464675222ac38786d2cce0b349fbfb826b
niuminguo <niuminguo@shequcun.com>
2015-08-27 14:49:30 +0800

    fixed

--- hyperdrive/db/__init__.py
+++ hyperdrive/db/__init__.py
@@ -1,15 +1,15 @@
 from hyperdrive.db.mongodb import MongoAPI
 from hyperdrive.common import cfg
 
-CONF = cfg.CONF
+conf = cfg.CONF
 
 
 class API(MongoAPI):
     def __init__(self):
 
-        super(API, self).__init__(CONF.mongo_host,
-                                  CONF.mongo_port,
-                                  CONF.mongo_db)
+        super(API, self).__init__(conf.mongo_host,
+                                  conf.mongo_port,
+                                  conf.mongo_db)
 
     def __getattr__(self, item):
         return getattr(self, item)
