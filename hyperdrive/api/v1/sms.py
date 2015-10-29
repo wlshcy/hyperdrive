cafd3ae7fb78936d1f4954ee572a3b0bdb982f41
pwzgorilla <pwzgorilla@gmail.com>
2015-10-29 16:04:55 +0800

    add logger and modified create method

--- hyperdrive/api/v1/sms.py
+++ hyperdrive/api/v1/sms.py
@@ -3,30 +3,35 @@ __author__ = 'nmg'
 
 from hyperdrive.base import Base
 from hyperdrive import wsgi
-from hyperdrive.common.response import HttpResponse
+from hyperdrive.common.response import Response
 import random
+from hyperdrive.common import log as logging
+import time
+
+logger = logging.getLogger(__name__)
 
 
 class Controller(Base):
     def __init__(self):
         super(Controller, self).__init__()
 
-    def create(self, req, body):
+    def create(self, req):
         """
         Generate sms code.
         """
-        mobile = body.pop('mobile')
+        mobile = req.POST['mobile']
 
         code = '%06d' % random.randint(0, 999999)
         content = code
         try:
             self.sms(mobile, content)
-            log.info('send sms: %s, %s' % (mobile, content))
+            logger.info('send sms: %s, %s' % (mobile, content))
         except Exception as e:
-            log.error('send sms failed: %s, %s, %s' % (mobile, content, e))
+            logger.error('send sms failed: %s, %s, %s' % (mobile, content, e))
 
         self.cache.set(mobile, {'code': code, 'time': round(time.time())}, 5)
-        return HttpResponse({"token": body['account']+body['password']})
+
+        return Response(201)
 
 
 def create_resource():
